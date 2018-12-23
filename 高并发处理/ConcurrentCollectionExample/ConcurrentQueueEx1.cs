40e5353a4ad51b95b9ca6a6650aa968c83b6db3c
yungchu <jianlive@qq.com>
2018-12-23 22:30:01 +0800

    添加并发集合处理工程

---
+++ 高并发处理/ConcurrentCollectionExample/ConcurrentQueueEx1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentCollectionExample
+{
+    /// <summary>
+    /// https://blog.csdn.net/q3585914/article/details/79231268 
+    /// </summary>
+    public class ConcurrentQueueEx1
+    {
+        //private static readonly IList<Product> Products = new List<Product>();
+        private static readonly ConcurrentQueue<Product> Products = new ConcurrentQueue<Product>();
+
+        public static void  GetProductsCount()
+        {
+           var task1= Task.Factory.StartNew(AddProducts);
+           var task2 = Task.Factory.StartNew(AddProducts);
+           var task3 = Task.Factory.StartNew(AddProducts);
+           Task.WaitAll(task1, task2, task3);//同步执行
+
+          Console.WriteLine(Products.Count);
+           
+        }
+
+        private static void AddProducts()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Product product = new Product {Name = "name" + i, Category = "Category" + i, SellPrice = i};
+                // Products.Add(product);
+                Products.Enqueue(product);
+            }
+
+            Task.Delay(1000);
+        }
+    }
+
+    public  class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int SellPrice { get; set; }
+    }
+
+}
---
+++ 高并发处理/ConcurrentCollectionExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConcurrentCollectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********并发集合使用***************");
+            // ConcurrentQueue
+            ConcurrentQueueEx1.GetProductsCount();
+            Console.ReadKey();
+        }
+    }
+
+
+}
